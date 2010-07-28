5154b826a862774c753308797362a10c61973147
GordonDiggs <gordon@gordondiggs.com>
2010-07-27 23:54:13 -0400

    Added some documentation

--- jquery.jincss.js
+++ jquery.jincss.js
@@ -1,8 +1,15 @@
 (function($,undefined){
+  
   $.fn.jincss = function(attribute, amount){
+    
+    // if a string such as '3' or '3px' is passed in, 
+    // handle it properly
     if(typeof amount == 'string'){
       amount = parseInt(amount);
     }
+    
+    // use anonymous function syntax to update the value of the style
     $(this).css(attribute, function(i, cur_val){ return parseInt(cur_val) + amount; });
   }
+  
 })(jQuery);
